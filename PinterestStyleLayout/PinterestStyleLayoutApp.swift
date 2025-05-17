4c0410697b6a71f174d6ba79ba5962f5b273d8e9
belkhadir <belkhadir.anas@gmail.com>
2025-05-17 13:38:03 +0100

    Initial Commit

---
+++ PinterestStyleLayout/PinterestStyleLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestStyleLayoutApp.swift
+//  PinterestStyleLayout
+//
+//  Created by Belkhadir Anas on 17/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct PinterestStyleLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
